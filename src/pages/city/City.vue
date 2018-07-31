94fff21cfe5f99f872ed6fce303b21f86c013278
iwen <1044803551@qq.com>
2018-07-31 23:32:19 +0800

    add list to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,16 +2,18 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
     </div>
 </template>
 
 <script>
 import CityHeader from "./components/Header"
 import CitySearch from "./components/Search"
+import CityList from "./components/List"
     export default {
         name:"City",
         components: {
-            CityHeader,CitySearch,
+            CityHeader,CitySearch,CityList
         }
     }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="list">
+        <div class="area">
+            <div class="title border-topbottom">当前城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">热门城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+            </div>            
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+                <div class="item border-bottom">阿拉尔</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"CityList"
+    }
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+.border-topbottom
+    &:before
+        border-color #cccccc
+    &:after
+        border-color #cccccc
+
+.border-bottom
+    &:before
+        border-color #cccccc
+
+.list
+    position absolute
+    overflow hidden
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background red
+    .title
+        line-height .4rem
+        background #eeeeee
+        padding left .2rem
+        color #666666
+        font-size .26rem
+        padding-left .1rem
+    .button-list
+        padding .1rem .6rem .1rem .1rem
+        overflow hidden
+        .button-wrapper
+            width 33.33%
+            float left 
+            .button
+                text-align center
+                margin .1rem
+                padding .1rem 0
+                border-radius .06rem
+                border .02rem solid #cccccc
+    .item-list
+        .item
+            line-height .76rem
+            padding-left .2rem
+</style>
