c2794475e76ed41f4ec83b83204a2beea76ba988
iwen <1044803551@qq.com>
2018-08-02 15:23:32 +0800

    add search-logic to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,56 +1,56 @@
 <template>
     <div>
         <city-header></city-header>
-        <city-search></city-search>
+        <city-search :cities="cities"></city-search>
         <city-list :letter="letter" :cities="cities" :hot="hotCities"></city-list>
         <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
     </div>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
-import CityHeader from "./components/Header"
-import CitySearch from "./components/Search"
-import CityList from "./components/List"
-import CityAlphabet from "./components/Alphabet"
+import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
+import CityList from "./components/List";
+import CityAlphabet from "./components/Alphabet";
 
-    export default {
-        name:"City",
-        components: {
-            CityHeader,CitySearch,CityList,CityAlphabet
-        },
-        data() {
-            return {
-                cities: {},
-                hotCities:[],
-                letter:''
-            }
-        },
-        methods: {
-            getCityInfo(){
-                axios.get('/api/city.json')
-                .then(res=>{
-                    console.log(res)
-                    if(res.status === 200){
-                        if(res.data.ret&&res.data.data){
-                            const data = res.data.data
-                            this.cities = data.cities
-                            this.hotCities = data.hotCities
-                        }
+export default {
+    name: "City",
+    components: {
+        CityHeader,
+        CitySearch,
+        CityList,
+        CityAlphabet
+    },
+    data() {
+        return {
+            cities: {},
+            hotCities: [],
+            letter: ""
+        };
+    },
+    methods: {
+        getCityInfo() {
+            axios.get("/api/city.json").then(res => {
+                if (res.status === 200) {
+                    if (res.data.ret && res.data.data) {
+                        const data = res.data.data;
+                        this.cities = data.cities;
+                        this.hotCities = data.hotCities;
                     }
-                })
-            },
-            handleLetterChange(letter){
-                this.letter = letter
-            }
+                }
+            });
         },
-        mounted () {
-            this.getCityInfo()
+        handleLetterChange(letter) {
+            this.letter = letter;
         }
+    },
+    mounted() {
+        this.getCityInfo();
     }
+};
 </script>
 
 <style lang="stylus" scoped>
-
 </style>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,28 +1,92 @@
 <template>
-    <div class="search">
-        <input type="text" class="search-input" placeholder="输入城市名或拼音">
+    <div>
+        <div class="search">
+            <input v-model="keyword" type="text" class="search-input" placeholder="输入城市名或拼音">
+        </div>
+        <div v-show="keyword" class="search-content" ref="search">
+            <ul>
+                <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+                <li v-show="hasNoData" class="search-item border-bottom">没有找到匹配的数据</li>
+            </ul>
+        </div>
     </div>
+
 </template>
 
 <script>
-    export default {
-        name:'CitySearch'
+import Bsrcoll from "better-scroll"
+export default {
+    name: "CitySearch",
+    props:{
+        cities:Object
+    },
+    data() {
+        return {
+            keyword:"",
+            list:[],
+            timer:null
+        }
+    },
+    computed: {
+        hasNoData(){
+            return !this.list.length
+        }
+    },
+    watch: {
+        keyword(){
+            if(this.timer){
+                clearTimeout(this.timer)
+            }
+            if(!this.keyword){
+                this.list = []
+                return
+            }
+            this.timer = setTimeout(()=>{
+                const result = []
+                for(let i in this.cities){
+                    this.cities[i].forEach((value) => {
+                        if(value.spell.indexOf(this.keyword)>-1 || value.name.indexOf(this.keyword)>-1){
+                            result.push(value)
+                        }
+                    });
+                    this.list = result
+                }
+            },100)
+        }
+    },
+    mounted () {
+        this.srcool = new Bsrcoll(this.$refs.search)
     }
+};
 </script>
 
 <style scoped lang="stylus">
-@import "~styles/varibles.styl";
+@import '~styles/varibles.styl'
 .search
-    height .72rem
-    padding 0 .1rem
+    height 0.72rem
+    padding 0 0.1rem
     background $bgColor
     .search-input
         box-sizing border-box
         width 100%
-        height .62rem
-        line-height .62rem
+        height 0.62rem
+        line-height 0.62rem
         text-align center
-        border-radius .06rem
+        border-radius 0.06rem
         color #666
-        padding 0 .1rem
+        padding 0 0.1rem
+.search-content       
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
+    background #eeeeee 
+    z-index 1
+    .search-item
+        line-height .62rem
+        padding-left .2rem
+        color #666
+        background #ffffff
 </style>
