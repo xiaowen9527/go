0761d617d79f6891b7ccc0f82870276c474c9c40
iwen <1044803551@qq.com>
2018-08-06 00:12:16 +0800

    banner finish

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container" @click="handleGallaryClick">
+        <div class="wrapper">
+            <swiper :options="swiperOptions">
+                <swiper-slide v-for="(item,index) in imgs" :key="item.index">
+                    <img class="gallary-img" :src="item" />
+                </swiper-slide>
+                <div class="swiper-pagination"  slot="pagination"></div>
+            </swiper>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"CommonGallary",
+        props:{
+            imgs:{
+                type:Array,
+                default(){
+                    return []
+                }
+            }
+        },
+        data() {
+            return {
+                swiperOptions: {
+                    pagination: '.swiper-pagination',
+                    paginationType: 'fraction',
+                    observeParents: true,
+                    observer: true
+                }
+            }
+        },
+        methods: {
+            handleGallaryClick(){
+                this.$emit('close')
+            }
+        }
+
+    }
+</script>
+
+<style scoped lang="stylus">
+.container >>> .swiper-container
+    overflow: inherit
+.container >>> .swiper-pagination-bullet-active
+    background: #fff    
+.container
+    position fixed
+    display flex
+    flex-direction column
+    justify-content center
+    top 0
+    bottom 0
+    left 0
+    right 0
+    background #000
+    z-index 99
+    .wrapper
+        background #000
+        width 100%
+        height 0
+        overflow hidden
+        padding-bottom 65%
+        .gallary-img
+            width 100%
+    .swiper-pagination
+        color: #000
+        top: .2rem 
+        font-size .32rem              
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,19 +1,45 @@
 <template>
-    <div class="banner">
-        <img class="banner-img" src="https://img1.qunarzz.com/p/tts2/1808/3a/2b8fa80a3815de02.jpg_r_640x420x90_8b6ef55c.jpg">
-        <div class="banner-info">
-            <div class="banner-title">产品编号2122620764</div>
-            <div class="banner-num">
-                <span class="iconfont banner-icon">&#xe601;</span>
-                39
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="https://img1.qunarzz.com/p/tts2/1808/3a/2b8fa80a3815de02.jpg_r_640x420x90_8b6ef55c.jpg">
+            <div class="banner-info">
+                <div class="banner-title">产品编号2122620764</div>
+                <div class="banner-num">
+                    <span class="iconfont banner-icon">&#xe601;</span>
+                    39
+                </div>
             </div>
         </div>
+        <common-gallary @close="handleGallaryClose" :imgs="imgs" v-show="showGallary"></common-gallary>
     </div>
 </template>
 
 <script>
+import CommonGallary from "common/gallary/Gallary";
     export default {
-        name:"DetailBanner"
+        name:"DetailBanner",
+        data() {
+            return {
+                showGallary:false,
+                imgs:[
+                    'https://img1.qunarzz.com/p/tts2/1808/3a/2b8fa80a3815de02.jpg_r_640x420x90_8b6ef55c.jpg','https://img1.qunarzz.com/p/tts1/1808/7e/bf3ce7d3a7d62702.jpg_r_1280x840x90_e42d33ed.jpg','https://img1.qunarzz.com/p/tts8/1807/ef/7eeaf07f984c802.jpg_r_1280x840x90_efb0133d.jpg'
+                ]
+            }
+        },
+        methods: {
+          handleBannerClick(){
+              this.showGallary = true
+          },
+          handleGallaryClose(){
+              this.showGallary = false
+          },
+        },
+        computed: {
+           
+        },
+        components: {
+            CommonGallary
+        }
     }
 </script>
 
